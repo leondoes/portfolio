b7ab48c194ac9f1fd326b09d25851337fab6055c
Leon Dobrescu <50689177+leondoes@users.noreply.github.com>
2023-11-18 04:08:23 -0500

    :art:Added a rounded border to video player to fit with rest of content (#71)

--- src/common/AutoplayVideo/styled.js
+++ src/common/AutoplayVideo/styled.js
@@ -1,20 +1,26 @@
 import styled from "styled-components";
 
 
-export const VideoContainer = styled.div`
-  width: 100%;
-  padding-top: 100%;
-  position: relative;
-  overflow: hidden;
-  opacity: ${({ isVideoLoaded }) => (isVideoLoaded ? '1' : '0')};
-  transition: opacity 0.3s ease;
+export const Video = styled.video`
+     position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    border-radius: 18px; // Ensures the video has rounded corners
+    object-fit: cover; // Ensures the video content covers the player area
+    overflow: hidden;
 `;
 
-export const Video = styled.video`
-  width: 100%;
-  height: 100%;
-  position: absolute;
-  top: 0;
-  left: 0;
-  object-fit: scale-down;
+export const VideoContainer = styled.div`
+   width: 100%;
+  position: relative;
+  overflow: hidden;
+  padding-top: 90%; // Adjust this percentage to match the video content's aspect ratio
+  border-radius: 18px;
+  -moz-box-shadow: 0 0 10px #5d5f61;
+  -webkit-box-shadow: 0 0 10px #5d5f61;
+  box-shadow: 0 0 10x #5d5f61;
+  opacity: ${({ isVideoLoaded }) => (isVideoLoaded ? '1' : '0')};
+  transition: opacity 0.3s ease;
 `;
