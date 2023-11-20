23df91061176dd6862bab70ca3e297838e37bc16
Leon Dobrescu <50689177+leondoes@users.noreply.github.com>
2023-11-20 08:24:33 -0500

    :fire: Removed unused code (scrolUtils.js) (#73)

--- src/utils/scrollUtils.js
+++
@@ -1,8 +0,0 @@
-export const scrollToBottomOfElement = (elementRef) => {
-    const bottomOfElement = elementRef.current.getBoundingClientRect().bottom + window.pageYOffset;
-    window.scrollTo({
-      top: bottomOfElement,
-      behavior: 'smooth'
-    });
-  };
-  
